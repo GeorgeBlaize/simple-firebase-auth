fbd283c7ca788eac1f362908480759673e0e78c8
Student ApnaCollege.in <92225843+GeorgeBlaize@users.noreply.github.com>
2025-05-12 20:34:05 +0600

    added display user info and sign out

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,12 @@
 
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import React from 'react';
+import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import React, { useState } from 'react';
 import { auth } from '../../firebase/firebase.init';
 
 
 const Login = () => {
+
+    const [user,setUser] =useState(null)
     
     const provider = new GoogleAuthProvider();
     
@@ -13,16 +15,41 @@ const Login = () => {
 
         signInWithPopup(auth,provider).then(result => {
             console.log(result)
+            setUser(result.user);
         }).catch(error =>{
             console.log(error);
         })
 
        
     }
+
+    const handleSignOut = () =>{
+        signOut(auth).then(() =>{
+            console.log("signout completed");
+            setUser(null)
+        }).catch(error =>{
+            console.log(error)
+        })
+    }
     return (
         <div>
             <h2>Please Login</h2>
-            <button onClick={handleGoogleSignIn}>Sign In With Google</button>
+            {/* <button onClick={handleGoogleSignIn}>Sign In With Google</button>
+            <button onClick={handleSignOut}>Sign Out</button> */}
+            {
+                user ? 
+                <button onClick={handleSignOut}>Sign Out</button> : 
+                <button onClick={handleGoogleSignIn}>Sign In With Google</button>
+            }
+
+            {   
+                user && 
+                <div>
+                <h3>{user.displayName}</h3>
+                <p>{user.email}</p>
+                <img src={user.photoURL} alt="" />
+            </div>
+            }
         </div>
     );
 };
