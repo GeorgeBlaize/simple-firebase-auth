70edd7dfa5921d90fc2e2f0a0916bb2626780ca7
Student ApnaCollege.in <92225843+GeorgeBlaize@users.noreply.github.com>
2025-05-12 19:38:54 +0600

    added Google sign in

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,23 @@
+
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import React from 'react';
+import { auth } from '../../firebase/firebase.init';
+
 
 const Login = () => {
-
+    
+    const provider = new GoogleAuthProvider();
+    
     const handleGoogleSignIn = () =>{
-        console.log('google sign in clicked')
+        console.log('google sign in clicked');
+
+        signInWithPopup(auth,provider).then(result => {
+            console.log(result)
+        }).catch(error =>{
+            console.log(error);
+        })
+
+       
     }
     return (
         <div>
--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -1,22 +1,22 @@
+//DONOT SHARE FIREBASE CONFIG PUBLICALLY
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD-xrmU-K6iroUS383W6nRFYUwAZQ9bZF8",
-  authDomain: "simple-firebase-auth-af364.firebaseapp.com",
-  projectId: "simple-firebase-auth-af364",
-  storageBucket: "simple-firebase-auth-af364.firebasestorage.app",
-  messagingSenderId: "958467044567",
-  appId: "1:958467044567:web:117b6db2a0e99d607dec92"
+  apiKey: "AIzaSyDluLG3wgT_FR8JOZMyTYcJO63urlJfwNc",
+  authDomain: "simple-firebase-auth2-1a230.firebaseapp.com",
+  projectId: "simple-firebase-auth2-1a230",
+  storageBucket: "simple-firebase-auth2-1a230.firebasestorage.app",
+  messagingSenderId: "963080459281",
+  appId: "1:963080459281:web:ff69e706ec91a1b6310db0"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Authentication and get a reference to the service
-export const auth = getAuth(app);
+export const auth =getAuth(app);
